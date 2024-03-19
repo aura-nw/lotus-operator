cc0805ce46cf245853804c6d932233bfce503b35
htiennv <tien.nv291997@gmail.com>
2024-03-19 14:14:05 +0700

    Add interfaces

--- internal/operator/evm.go
+++ internal/operator/evm.go
@@ -3,9 +3,25 @@ package operator
 import (
 	"math/big"
 
+	"github.com/aura-nw/btc-bridge-core/clients/evm/contracts"
+	"github.com/aura-nw/btc-bridge-core/clients/evm/txmgr"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type EvmVerifier interface {
-	VerifyIncomingInvoice(id string, utxo string, amount *big.Int, recipient common.Address) bool
+	VerifyIncomingInvoice(id uint64, utxo string, amount *big.Int, recipient common.Address) (bool, error)
+	GetLastIdVerifyIncomingInvoice(operator common.Address) (uint64, error)
+	GetIncomingInvoiceCount() (uint64, error)
+	GetIncomingInvoice(id uint64) (*contracts.IGatewayIncomingInvoiceResponse, error)
+
+	VerifyOutgoingInvoice(id uint64, amount *big.Int, recipient common.Address, signature string) (bool, error)
+	GetLastIdVerifyOutgoingInvoice(operator common.Address) (uint64, error)
+	GetOutgoingInvoiceCount() (uint64, error)
+	GetOutgoingInvoice(id uint64) (*contracts.IGatewayOutgoingInvoiceResponse, error)
+}
+
+type EvmSender interface {
+	GetAddress() common.Address
+	SendAndWait(tx txmgr.TxCandidate) (*types.Receipt, error)
 }
--- internal/operator/operator.go
+++ internal/operator/operator.go
@@ -3,8 +3,10 @@ package operator
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/aura-nw/btc-bridge-operator/config"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 type Operator struct {
@@ -15,6 +17,7 @@ type Operator struct {
 	config *config.Config
 
 	evmVerifier EvmVerifier
+	evmSender   EvmSender
 }
 
 func NewOperator(ctx context.Context, config *config.Config, logger *slog.Logger) (*Operator, error) {
@@ -29,13 +32,66 @@ func NewOperator(ctx context.Context, config *config.Config, logger *slog.Logger
 }
 
 func (op *Operator) Start() {
-
+	go op.incomingEventsLoop()
+	go op.outgoingEventsLoop()
 }
 
-func (op *Operator) readContractEvents() error {
-	return nil
+func (op *Operator) incomingEventsLoop() {
+	lastId, err := op.evmVerifier.GetLastIdVerifyIncomingInvoice(op.evmSender.GetAddress())
+	if err != nil {
+		op.logger.Error("incomingEventsLoop: get last id failed", "err", err)
+		return
+	}
+	op.logger.Info("incomingEventsLoop", "last_id", lastId)
+
+	ticker := time.NewTicker(time.Duration(op.config.Evm.QueryInterval) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-op.ctx.Done():
+			op.logger.Info("incomingEventsLoop: context done")
+			return
+		case <-ticker.C:
+			count, err := op.evmVerifier.GetIncomingInvoiceCount()
+			if err != nil {
+				op.logger.Error("incomingEventsLoop: get incoming invoice count failed", "err", err)
+				continue
+			}
+			if lastId == count {
+				op.logger.Info("incomingEventsLoop: waiting for next incoming id", "err", err)
+				continue
+			}
+			// Process next id
+			invoice, err := op.evmVerifier.GetIncomingInvoice(lastId + 1)
+			if err != nil {
+				op.logger.Error("incomingEventsLoop: get incoming invoice failed", "id", lastId+1, "err", err)
+				continue
+			}
+			if IsInAddresses(invoice.Verifieds, op.evmSender.GetAddress()) {
+				op.logger.Info("incomingEventsLoop: operator has verified", "address", op.evmSender.GetAddress().Hex())
+				lastId++
+				continue
+			}
+
+			lastId++
+		}
+
+	}
+}
+
+func (op *Operator) outgoingEventsLoop() {
 }
 
 func (op *Operator) Stop() {
 	op.cancel()
+}
+
+func IsInAddresses(addresses []common.Address, target common.Address) bool {
+	for _, addr := range addresses {
+		if target.Cmp(addr) == 0 {
+			return true
+		}
+	}
+	return false
 }
